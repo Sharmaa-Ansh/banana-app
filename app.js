6247c0751762c8a70458cbac6ae197bc2aea959e
Ansh Sharma <82943199+Sharmaa-Ansh@users.noreply.github.com>
2021-05-21 19:18:43 +0530

    javacript done program runs

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var btntranslate = document.querySelector("#btn-translate");
 var outputbar= document.querySelector("#output-bar")
 var searchbox= document.querySelector("#search-bar")
-function clickHandler(){
-    console.log('click');
-    console.log('input', searchbox.value);
-    outputbar.innerText = searchbox.value
+
+
+
+var serverURl= "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURl + "?" + "text=" + text
 }
+
+
+
+function clickHandler(){
+    var text = searchbox.value
+    fetch(getTranslationURL(text))
+    .then(response=> response.json())
+    .then(json=> {
+        var translatedtext= json.contents.translated
+        outputbar.innerText = translatedtext
+    })
+};
 
 
 btntranslate.addEventListener("click",  clickHandler);
